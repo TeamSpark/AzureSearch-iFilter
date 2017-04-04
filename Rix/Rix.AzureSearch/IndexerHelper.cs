c46d58ac32eec54bce4074febd6afbbff01b304a
BoykoAnt <boyko.ant@gmail.com>
2017-04-04 12:04:01 +0000

    started with azure search indexer helper

---
+++ Rix/Rix.AzureSearch/IndexerHelper.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.Azure.Search;
+using Microsoft.Azure.Search.Models;
+using Rix.Configuration;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Rix.AzureSearch
+{
+	public class IndexerHelper
+	{
+		protected readonly SearchServiceClient ServiceClient;
+
+		public IndexerHelper()
+		{
+			ServiceClient = new SearchServiceClient(ConfigurationReader.SearchServiceName, new SearchCredentials(ConfigurationReader.SearchServiceApiKey));
+		}
+
+		public async Task EnsureIndexerCreatedAsync()
+		{
+			var field = new Field();
+			field.IsFacetable = false;
+			field.IsFilterable = false;
+			field.IsKey = false;
+			field.IsRetrievable = true;
+			field.IsSearchable = false;
+			field.IsSortable = false;
+			field.Name = ConfigurationReader.SearchFieldContentName;
+			field.Type = DataType.String;
+
+			var index = new Index();
+			index.Name = ConfigurationReader.SearchIndexName;
+			index.Fields = new List<Field>();
+			index.Fields.Add(field);
+
+			var dataSource = new DataSource();
+			dataSource.Name = ConfigurationReader.SearchDataSourceName;
+			dataSource.Credentials = new DataSourceCredentials(ConfigurationReader.BlobStorageCopyConnectionString);
+			dataSource.Type = DataSourceType.AzureBlob;
+			dataSource.Container = new DataContainer(ConfigurationReader.BlobStorageCopyContainerName);
+
+			await Task.WhenAll(
+				ServiceClient.DataSources.CreateOrUpdateAsync(dataSource),
+				ServiceClient.Indexes.CreateOrUpdateAsync(index));
+
+			var fieldMapping = new FieldMapping();
+			fieldMapping.SourceFieldName = "content"; // source field name for azure blob content
+			fieldMapping.TargetFieldName = field.Name;
+
+			var indexer = new Indexer();
+			indexer.Name = ConfigurationReader.SearchIndexerName;
+			indexer.DataSourceName = dataSource.Name;
+			indexer.TargetIndexName = index.Name;
+			indexer.FieldMappings = new List<FieldMapping>();
+			indexer.FieldMappings.Add(fieldMapping);
+
+			await ServiceClient.Indexers.CreateOrUpdateAsync(indexer);
+		}
+	}
+}
---
+++ Rix/Rix.Configuration/ConfigurationReader.cs
@@ -0,0 +1,18 @@
+﻿namespace Rix.Configuration
+{
+	public static class ConfigurationReader
+	{
+		public static readonly string SearchServiceName = "";
+		public static readonly string SearchServiceApiKey = "";
+		public static readonly string SearchDataSourceName = "";
+		public static readonly string SearchIndexName = "";
+		public static readonly string SearchIndexerName = "";
+
+		public static readonly string SearchFieldContentName = "content";
+
+		public static readonly string BlobStorageMainConnectionString = "";
+		public static readonly string BlobStorageCopyConnectionString = "";
+		public static readonly string BlobStorageCopyContainerName = "";
+
+	}
+}

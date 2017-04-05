01a7b279e6c0c4d11663050256aa5d074ea6d048
Sergiy Poplavskyi <s.poplavskiy@outlook.com>
2017-04-05 10:55:03 +0300

    Cleanup and optimization

---
+++ Rix/Rix.AzureSearch/IndexDocument.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Azure.Search;
+using Microsoft.Azure.Search.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rix.AzureSearch
+{
+    [SerializePropertyNamesAsCamelCase]
+    public class IndexDocument
+    {
+        [Key]
+        [IsRetrievable(true)]
+        public string Id { get; set; }
+        [IsRetrievable(true)]
+        public string Content { get; set; }
+    }
+}
--- Rix/Rix.AzureSearch/IndexerHelper.cs
+++ Rix/Rix.AzureSearch/IndexerHelper.cs
@@ -5,11 +5,9 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using System.Linq;
 using System;
-using System.ComponentModel.DataAnnotations;
 
 namespace Rix.AzureSearch
 {
-    using SearchStringType = Microsoft.Azure.Search.Models.DataType;
     public class IndexerHelper
 	{
 		protected readonly SearchServiceClient ServiceClient;
@@ -21,33 +19,13 @@ namespace Rix.AzureSearch
 
 		public async Task EnsureIndexerCreatedAsync()
 		{
-            var fieldIndex = new Field();
-			fieldIndex.IsFacetable = false;
-			fieldIndex.IsFilterable = false;
-			fieldIndex.IsKey = true;
-			fieldIndex.IsRetrievable = true;
-			fieldIndex.IsSearchable = false;
-			fieldIndex.IsSortable = false;
-			fieldIndex.Name = "id";
-			fieldIndex.Type = SearchStringType.String;
+            var definition = new Index()
+            {
+                Name = "indexDocument",
+                Fields = FieldBuilder.BuildForType<IndexDocument>()
+            };
 
-			var fieldContent = new Field();
-			fieldContent.IsFacetable = false;
-			fieldContent.IsFilterable = false;
-			fieldContent.IsKey = false;
-			fieldContent.IsRetrievable = true;
-			fieldContent.IsSearchable = false;
-			fieldContent.IsSortable = false;
-			fieldContent.Name = ConfigurationReader.SearchFieldContentName;
-			fieldContent.Type = SearchStringType.String;
-
-			var index = new Index();
-			index.Name = ConfigurationReader.SearchIndexName;
-			index.Fields = new List<Field>();
-			index.Fields.Add(fieldIndex);
-			index.Fields.Add(fieldContent);
-
-			var dataSource = new DataSource();
+            var dataSource = new DataSource();
 			dataSource.Name = ConfigurationReader.SearchDataSourceName;
 			dataSource.Credentials = new DataSourceCredentials(ConfigurationReader.BlobStorageConnectionString);
 			dataSource.Type = DataSourceType.AzureBlob;
@@ -55,9 +33,9 @@ namespace Rix.AzureSearch
 
 			await Task.WhenAll(
 				ServiceClient.DataSources.CreateOrUpdateAsync(dataSource),
-				ServiceClient.Indexes.CreateOrUpdateAsync(index));
+				ServiceClient.Indexes.CreateOrUpdateAsync(definition));
 
-			var mappingFunctionId = new FieldMappingFunction();
+            var mappingFunctionId = new FieldMappingFunction();
 			mappingFunctionId.Name = "extractTokenAtPosition";
 			mappingFunctionId.Parameters = new Dictionary<string, object>();
 			mappingFunctionId.Parameters.Add("delimiter", ".");
@@ -65,58 +43,43 @@ namespace Rix.AzureSearch
 
 			var fieldMappingId = new FieldMapping();
 			fieldMappingId.SourceFieldName = "metadata_storage_name"; // source field name for azure blob name
-			fieldMappingId.TargetFieldName = fieldIndex.Name;
+			fieldMappingId.TargetFieldName = nameof(IndexDocument.Id);
 			fieldMappingId.MappingFunction = mappingFunctionId;
 
 			var fieldMappingContent = new FieldMapping();
 			fieldMappingContent.SourceFieldName = "content"; // source field name for azure blob content
-			fieldMappingContent.TargetFieldName = fieldContent.Name;
+			fieldMappingContent.TargetFieldName = nameof(IndexDocument.Content);
 
 			var indexer = new Indexer();
 			indexer.Name = ConfigurationReader.SearchIndexerName;
 			indexer.DataSourceName = dataSource.Name;
-			indexer.TargetIndexName = index.Name;
+			indexer.TargetIndexName = definition.Name;
 			indexer.Parameters = new IndexingParameters();
 			indexer.Parameters.DoNotFailOnUnsupportedContentType();
 			indexer.FieldMappings = new List<FieldMapping>();
 			indexer.FieldMappings.Add(fieldMappingContent);
 			indexer.FieldMappings.Add(fieldMappingId);
-			indexer.Schedule = new IndexingSchedule(TimeSpan.FromMinutes(5));
 
 			await ServiceClient.Indexers.CreateOrUpdateAsync(indexer);
 		}
 
-		public async Task<List<KeyValuePair<string, string>>> GetDocumentsAsync()
+		public async Task<List<object>> GetDocumentsCountAsync()
 		{
 			var searchResults = await ServiceClient.Indexes.GetClient(ConfigurationReader.SearchIndexName).Documents.SearchAsync("*");
-
-			var results = new List<KeyValuePair<string, string>>();
-
-			foreach (var document in searchResults.Results.Select(x => x.Document))
-			{
-				var id = document.Single(x => x.Key == "id").Value.ToString();
-				var content = document.Single(x => x.Key == "content").Value.ToString();
-
-				results.Add(new KeyValuePair<string, string>(id, content));
-			}
-
-			return results;
+			return searchResults.Results.SelectMany(x => x.Document.Where(y => y.Key == "id").Select(y => y.Value)).ToList();
 		}
 
-		public async Task DeleteDocumentsByIdsAsync(List<string> ids)
+		public async Task DeleteDocumentsByIdAsync(List<string> ids)
 		{
-			int maxBatchSize = 1000;
-			int pagesCount = (ids.Count / maxBatchSize) + (ids.Count % maxBatchSize > 0 ? 1 : 0);
+			throw new NotImplementedException();
 
-			for (var i = 0; i < pagesCount; i++)
-			{
-				var indexActions = ids
-					.Skip(i * maxBatchSize)
-					.Take(maxBatchSize)
-					.Select(x => IndexAction.Delete("id", x));
+			//var maxBatchSize = 1000;
+			//var pagesCount = ids.Count % maxBatchSize;
 
-				await ServiceClient.Indexes.GetClient(ConfigurationReader.SearchIndexName).Documents.IndexAsync(new IndexBatch(indexActions));
-			}
+			//var indexActions = new List<IndexAction>();
+			//indexActions.Add(IndexAction.Delete("id", id));
+
+			//await ServiceClient.Indexes.GetClient(ConfigurationReader.SearchIndexName).Documents.IndexAsync(new IndexBatch(indexActions));
 		}
 
 		public async Task RunIndexerAsync()
@@ -124,14 +87,6 @@ namespace Rix.AzureSearch
 			await ServiceClient.Indexers.RunAsync(ConfigurationReader.SearchIndexerName);
 		}
 
-        [SerializePropertyNamesAsCamelCase]
-        public class IndexDocument
-        {
-            [Key]
-            [IsRetrievable(true)]
-            public string id { get; set; }
-            [IsRetrievable(true)]
-            public string content { get; set; }
-        }
+        
 	}
 }
